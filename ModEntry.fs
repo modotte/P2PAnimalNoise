423712e1a1ae812131864148e0462642913fb61c
modotte <runecroon@gmail.com>
2022-10-24 02:24:34 +0800

    Initial commit

---
+++ ModEntry.fs
@@ -0,0 +1,20 @@
+namespace Literarium
+
+open System
+open Microsoft.Xna.Framework
+open StardewModdingAPI
+open StardewModdingAPI.Events
+open StardewModdingAPI.Utilities
+open StardewValley
+
+type public ModEntry() =
+    inherit Mod()
+
+    override this.Entry(helper: IModHelper) =
+        helper.Events.Input.ButtonPressed.Add(fun (e) -> this.OnButtonPressed(e))
+
+    member private this.OnButtonPressed(e: ButtonPressedEventArgs) =
+        if not Context.IsWorldReady then
+            ()
+        else
+            this.Monitor.Log($"{Game1.player.Name} pressed {e.Button}", LogLevel.Debug)
