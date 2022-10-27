86dcd59429169cbdec6e1ac630f5da479b525834
modotte <runecroon@gmail.com>
2022-10-27 12:08:08 +0800

    Minor code clean up

--- ModEntry.fs
+++ ModEntry.fs
@@ -1,21 +1,21 @@
 namespace P2PAnimalNoise
 
 open StardewModdingAPI
-open StardewModdingAPI.Events
 open StardewValley
 open GenericModConfigMenu
 open FSharp.Collections
 
 
+[<RequireQualifiedAccess>]
 module Constant =
     [<Literal>]
     let GenericModConfigMenuId = "spacechase0.GenericModConfigMenu"
 
-    let DefaultNoiseKind = "Duck's quack"
+    let Noise = "Duck's quack"
 
 type ModConfig() =
     member val NoiseButton: SButton = SButton.Q with get, set
-    member val NoiseKind: string = Constant.DefaultNoiseKind with get, set
+    member val Noise: string = Constant.Noise with get, set
 
 module Noise =
     let Animals =
@@ -44,45 +44,44 @@ type ModEntry() =
     inherit Mod()
     member val private config: ModConfig option = None with get, set
 
-    member private this.SetupConfig(e: GameLaunchedEventArgs) =
+    member private this.SetupConfig _ =
         let configMenu =
             this.Helper.ModRegistry.GetApi<IGenericModConfigMenuApi>(Constant.GenericModConfigMenuId)
 
         if configMenu.GetType() |> isNull then
             ()
+        else
+            configMenu.Register(
+                this.ModManifest,
+                (fun () -> this.config <- Some(ModConfig())),
+                (fun () -> this.Helper.WriteConfig(this.config.Value)),
+                false
+            )
 
-        configMenu.Register(
-            this.ModManifest,
-            (fun () -> this.config <- Some(new ModConfig())),
-            (fun () -> this.Helper.WriteConfig(this.config.Value)),
-            false
-        )
+            configMenu.AddKeybind(
+                this.ModManifest,
+                (fun () -> this.config.Value.NoiseButton),
+                (fun x -> this.config.Value.NoiseButton <- x),
+                (fun () -> "Noise keybind"),
+                (fun () -> "Bind a key button to play the sound in-game."),
+                ""
+            )
 
-        configMenu.AddKeybind(
-            this.ModManifest,
-            (fun () -> this.config.Value.NoiseButton),
-            (fun x -> this.config.Value.NoiseButton <- x),
-            (fun () -> "Noise keybind"),
-            (fun () -> "Bind a key button to play the sound in-game."),
-            ""
-        )
-
-        configMenu.AddTextOption(
-            this.ModManifest,
-            (fun () -> this.config.Value.NoiseKind),
-            (fun x -> this.config.Value.NoiseKind <- x),
-            (fun () -> "Noise"),
-            (fun () -> "Various (default) animal noises. Scroll down the list to see more options."),
-            Noise.Animals |> Map.keys |> Seq.toArray,
-            null,
-            ""
-        )
+            configMenu.AddTextOption(
+                this.ModManifest,
+                (fun () -> this.config.Value.Noise),
+                (fun x -> this.config.Value.Noise <- x),
+                (fun () -> "Noise"),
+                (fun () -> "Various (default) animal noises. Scroll down the list to see more options."),
+                Noise.Animals |> Map.keys |> Seq.toArray,
+                null,
+                ""
+            )
 
     override this.Entry(helper: IModHelper) =
         this.config <- this.Helper.ReadConfig<ModConfig>() |> Some
-        helper.Events.GameLoop.GameLaunched.Add(fun e -> this.SetupConfig(e))
-
+        helper.Events.GameLoop.GameLaunched.Add(this.SetupConfig)
         helper.Events.Input.ButtonPressed.Add(fun e ->
             if Context.IsPlayerFree then
                 if e.Button = this.config.Value.NoiseButton then
-                    Game1.currentLocation.playSound (Noise.Animals |> Map.find this.config.Value.NoiseKind))
+                    Game1.currentLocation.playSound (Noise.Animals |> Map.find this.config.Value.Noise))
